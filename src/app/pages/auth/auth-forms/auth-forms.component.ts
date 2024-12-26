18b67a2a09c182fa35fc25f907f4cafc54080735
NascimentoFrancisco <fmilk9049@gmail.com>
2024-12-26 11:40:02 -0300

    feat: Implementation of authentication and a snackbar

--- src/app/pages/auth/auth-forms/auth-forms.component.ts
+++ src/app/pages/auth/auth-forms/auth-forms.component.ts
@@ -4,22 +4,27 @@ import { DefaultInputTextComponent } from "../../../shared/default-input-text/de
 import { DefaultButtonComponent } from "../../../shared/default-button/default-button.component";
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
+import { SpinnerComponent } from "../../../shared/spinner/spinner.component";
+import { SnackbarService } from '../../../services/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-auth-forms',
   standalone: true,
   imports: [
     FormsModule,
-    HeaderNotAuthenticatedComponent, 
-    DefaultInputTextComponent, 
-    DefaultButtonComponent
-  ],
+    HeaderNotAuthenticatedComponent,
+    DefaultInputTextComponent,
+    DefaultButtonComponent,
+    SpinnerComponent
+],
   templateUrl: './auth-forms.component.html',
   styleUrl: './auth-forms.component.scss'
 })
 export class AuthFormsComponent {
 
   @Input() loginOrChangePassword: boolean = false;// If false, make login
+  public buttonStatus = true;
+  
 
   loginFormData = {
     username: "",
@@ -31,19 +36,26 @@ export class AuthFormsComponent {
     password2: "",
   }
 
-  constructor (private authService: AuthService) {}
+  constructor (
+    private authService: AuthService,
+    private snackBarService: SnackbarService
+  ) {}
 
   public onSubmit(){
     if(this.loginOrChangePassword){
-      this.changePassword();
+      if(this.changhePasswordFormData.password1 != "" && this.changhePasswordFormData.password2 != ""){
+        this.changePassword();
+      }
     }else{
-      this.handleLogin();
+      if(this.loginFormData.username !== "" && this.loginFormData.password !== ""){
+        this.handleLogin();
+      }
     }
 
   }
 
   public handleClick(){
-    console.log("Clicou!")
+    return;
   }
 
   private handleLogin(){
@@ -51,15 +63,23 @@ export class AuthFormsComponent {
       username: this.loginFormData.username,
       password: this.loginFormData.password
     }
+    this.buttonStatus = false;
     this.authService.login(loginRequest).subscribe({
       next: (response) => {
         if(response){
           console.log(response);
+          this.buttonStatus = true;
           this.cleanLoginFormData();
         }
       },
       error: (err) => {
         console.log(err);
+        this.buttonStatus = true;
+        if(err.status === 401){
+          this.handleErrors(err.error.detail, "error");
+        } else {
+          this.handleErrors(err.error.detail, "error");
+        }
       }
     })
 
@@ -72,6 +92,10 @@ export class AuthFormsComponent {
   private cleanLoginFormData(){
     this.loginFormData.username = "";
     this.loginFormData.password = "";
+  }
+
+  private handleErrors(message: string, type: 'success' | 'error' | 'warning' | 'info'): void{
+    this.snackBarService.show(message, type);
   }
 
 }
