9de2837f71c27e0465a4c29c732c9fd592d7624a
NascimentoFrancisco <fmilk9049@gmail.com>
2025-01-16 17:38:32 -0300

    feat: Implementing search functionality in the header

--- src/app/pages/home/components/list-products/list-products.component.ts
+++ src/app/pages/home/components/list-products/list-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CategoryResponse } from '../../../../interfaces/responses/categories/categoryResponse'
 import { ProductsResponse } from '../../../../interfaces/responses/products/productsResponse';
 import { ProductsService } from '../../../../services/products/products.service';
@@ -7,6 +7,7 @@ import { ShortenPipe } from '../../../../utils/pipes/shorten/shorten.pipe';
 import { CurrencyBrPipe } from "../../../../utils/pipes/currencybr/currency-br.pipe";
 import { SpinnerPageInfoComponent } from "../../../../shared/spinner-info/spinner-page-info.component";
 import { Router } from '@angular/router';
+import { SnackbarService } from '../../../../services/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-list-products',
@@ -21,15 +22,18 @@ import { Router } from '@angular/router';
   styleUrl: './list-products.component.scss',
   providers: [ShortenPipe]
 })
-export class ListProductsComponent implements OnInit{
+export class ListProductsComponent implements OnInit, OnChanges{
   @Input() categories?: CategoryResponse[];
   @Input() category?: string;
+  @Input() searchValue? = "";
   @Input() name?: string;
   public productsByCategories: Array<{ category: string, producs: Array<ProductsResponse> }> = [];
+  public productsBySearchValue: Array<{ category: string, producs: Array<ProductsResponse> }> = [];
 
   constructor(
     private proudctsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private snackbarService: SnackbarService
   ) {}
 
   ngOnInit(): void {
@@ -38,6 +42,13 @@ export class ListProductsComponent implements OnInit{
         this.getProductByCategory(category.slug, category.title);
       });
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['searchValue'] && this.searchValue) {
+      this.getProductsBySearchValue();
+    }
+    
   }
   
   public getProductByCategory(categorySlug: string, categoryTitle:string){
@@ -50,11 +61,39 @@ export class ListProductsComponent implements OnInit{
                 category: categoryTitle,
                 producs: response
               }
+            );
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    }
+  }
+
+  private getProductsBySearchValue(){
+    if(this.searchValue){
+      this.proudctsService.getProduct(this.searchValue).subscribe({
+        next: (response) => {
+          console.log(response);
+          if(response && response.length > 0){
+            this.productsBySearchValue.push(
+              {
+                category: this.searchValue!,
+                producs: response
+              }
+            );
+          } else {
+            this.snackbarService.show(
+              `Nenhum resultado na busca por "${this.searchValue}"`, "info", 4000
             )
           }
         },
         error: (err) => {
           console.log(err);
+          this.snackbarService.show(
+            "Erro fazer pesquisa", "error"
+          )
         }
       })
     }
